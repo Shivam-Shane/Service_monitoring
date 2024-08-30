868232ed49c4bee1b5d1f6914fa7f628e69924ca
Shivam-Shane <sk0551460@gmail.com>
2024-08-30 18:32:47 +0530

    updated name, added custom module so it will install without any issue, changes url & license, read file encoding changed to utf-08

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 def get_requirements(file_path: str):
     requirements = []
-    with open(file_path) as file_obj:
+    with open(file_path,encoding="utf-8") as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
 
@@ -11,18 +11,19 @@ def get_requirements(file_path: str):
     return requirements
 
 setup(
-    name="gmail_monitoring",
+    name="gmailmonitoring",
     version="1.0.1",
     packages=find_packages(),
+    py_modules=["logger",'util'], 
     author="SHIVAM",
     author_email="sk0551460@gmail.com",
-    description="A package for monitoring Gmail messages, and works as per standard", 
+    description="A package for monitoring Gmail messages, and works as per standard",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Shivam-Shane/gmail_monitoring..git",
+    url="https://github.com/Shivam-Shane/Service_monitoring",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License ::  GNU GENERAL PUBLIC LICENSE",
+        "License ::  GPL-3.0",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.10.3',
