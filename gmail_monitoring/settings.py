8b6351f8cf7f0c6cd7784be1e6942431a4e0b222
Shivam-Shane <sk0551460@gmail.com>
2024-09-04 20:51:39 +0530

    updated allowed host to localhost and deployment.deploy.com, for production IP

--- gmail_monitoring/settings.py
+++ gmail_monitoring/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-t*_d^l7*biw^8(te-8bkyo&kzpkcfr^d(z6js@&rm*$s)^f3^w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','deployment.deploy.com']
 
 
 # Application definition
