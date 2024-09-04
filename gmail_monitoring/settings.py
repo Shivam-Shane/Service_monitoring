137069d92ec3c147d58af3e8b8745438088b5a1a
Shivam-Shane <sk0551460@gmail.com>
2024-09-04 20:47:12 +0530

    Debug changed to false from true

--- gmail_monitoring/settings.py
+++ gmail_monitoring/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-t*_d^l7*biw^8(te-8bkyo&kzpkcfr^d(z6js@&rm*$s)^f3^w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
