f536ae92222dc70197487a1d6be9e155865d718e
Shivam-Shane <sk0551460@gmail.com>
2024-09-05 03:47:55 +0530

    updated comments, added file read call to util function, added some prechecks to ensure data is present before creating any plots, removed unnessary prechecks at 'email_sent_per_day_graph'

--- gmail_graph.py
+++ gmail_graph.py
@@ -4,13 +4,15 @@ import seaborn as sns
 import io
 from logger import logging
 import matplotlib
+from util import read_yaml
 
 class GmailGraph:
     def __init__(self):
-        matplotlib.use('Agg')
+        matplotlib.use('Agg') # Using Agg so there is no issue of main thread execution that matplotlib accepts
+        self.data_config=read_yaml("config.yaml")
 
     def load_data(self): # For loading the dataset
-        return pd.read_csv("emails.csv")
+        return pd.read_csv(self.data_config.get('CSV_FILE'))
     
     def plot_graph(self):
         try:
@@ -30,41 +32,42 @@ class GmailGraph:
     def email_sent_per_day_graph(self):
         # Load data from CSV file and convert 'Last Sent' to datetime format
         self.data=self.load_data()
+
+        if self.data['Last Sent'].empty:
+            logging.warning("DataFrame is empty. Skipping plot creation.")
+            return None
+        
         self.data['Last Sent']=pd.to_datetime(self.data['Last Sent'], errors='coerce') 
         self.data = self.data.dropna(subset=['Last Sent'])    # remove any null values
+        emails_per_day = self.data.groupby(self.data['Last Sent'].dt.date).size()
+        
+        fig, ax = plt.subplots(figsize=(8, 4))
+        # Plotting 
+        plt.plot(emails_per_day.index, emails_per_day.values, marker='o', linestyle='-', color='b')
+        plt.title('Number of Emails Sent Per Day')
+        plt.xlabel('Date')
+        plt.ylabel('Number of Emails')
+        plt.xticks(rotation=45)
+        plt.tight_layout()
+        # Save the plot to a BytesIO object
+        img_buffer = io.BytesIO()
+        plt.savefig(img_buffer, format='png')
+        img_buffer.seek(0)
+        plt.close(fig)
 
-        if int(self.data['Last Sent'].value_counts().sum()) > 0: 
-            logging.debug("Trying to generate email sent per day plot.")
-            
-            emails_per_day = self.data.groupby(self.data['Last Sent'].dt.date).size()
-            
-            fig, ax = plt.subplots(figsize=(8, 4))
-            # Plotting 
-            plt.plot(emails_per_day.index, emails_per_day.values, marker='o', linestyle='-', color='b')
-            plt.title('Number of Emails Sent Per Day')
-            plt.xlabel('Date')
-            plt.ylabel('Number of Emails')
-            plt.xticks(rotation=45)
-            plt.tight_layout()
-            # Save the plot to a BytesIO object
-            img_buffer = io.BytesIO()
-            plt.savefig(img_buffer, format='png')
-            img_buffer.seek(0)
-            plt.close(fig)
+        # Free up memory
+        del self.data
 
-            # Free up memory
-            del self.data
-
-            logging.debug('Returning Number of Emails Sent Per Day chart in form of BytesIO')
-            return img_buffer
-
-        else: 
-            logging.warning("No data found to generate chart")
-            return None
+        logging.debug('Returning Number of Emails Sent Per Day chart in form of BytesIO')
+        return img_buffer
 
     def email_sent_for_per_service_graph(self):
         self.data=self.load_data()
 
+        if self.data['Last Sent'].empty:
+            logging.warning("DataFrame is empty. Skipping plot creation.")
+            return None
+        
         # Count the number of emails sent for each 'service'
         name_counts = self.data['Name'].value_counts()
         names=name_counts[0:5]
@@ -92,14 +95,22 @@ class GmailGraph:
 
     def heatmap_of_activity_graph(self):
         self.data=self.load_data()
+
+        if self.data['Last Sent'].empty:
+            logging.warning("DataFrame is empty. Skipping plot creation.")
+            return None  
+             
         self.data['Last Sent']=pd.to_datetime(self.data['Last Sent'], errors='coerce') # convert to datetime format
-        self.data = self.data.dropna(subset=['Last Sent'])
-        self.data['Hour'] = self.data['Last Sent'].dt.hour
-        self.data['Day'] = self.data['Last Sent'].dt.day_name()
+        self.data = self.data.dropna(subset=['Last Sent'])  # remove any null values
+        self.data['Hour'] = self.data['Last Sent'].dt.hour #Getting hour from dates
+        self.data['Day'] = self.data['Last Sent'].dt.day_name() # getting day from dates
         
         # Plotting
         fig, ax = plt.subplots(figsize=(8, 4))
         pivot_table = self.data.pivot_table(index='Hour', columns='Day', aggfunc='size', fill_value=0)
+        if pivot_table.empty:
+            logging.warning("No data to generate Heatmap. Skipping plot creation.")
+            return None
         sns.heatmap(pivot_table, cmap="YlGnBu", linewidths=.5, annot=True, fmt="d")
         plt.title('Heatmap of Email Activity by Hour and Day')
         plt.xlabel('Day of the Week')
