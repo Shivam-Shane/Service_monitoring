293e28f2386a3680620fcc75e337a50f50d48a42
Shivam-Shane <sk0551460@gmail.com>
2024-09-11 21:43:03 +0530

    ensuring retun type checking, while returing for start and stop view

--- gmail_monitoring/views.py
+++ gmail_monitoring/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, redirect,HttpResponse
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
 from util import update_yaml # type:ignore
 from gmail_graph import GmailGraph
 from logger import logging
@@ -6,6 +7,8 @@ from UI_view_handler import View_handler
 from io import BytesIO
 import base64
 from datetime import datetime
+from django.http import JsonResponse
+from django.template.loader import render_to_string
 
 class View_Functions():
     def __init__(self):
@@ -14,21 +17,21 @@ class View_Functions():
 
     def index_view(self,request):
         # Fetch configuration details, current batch, and emails to process from view handler
-        configuration_details,current_batch,emails_to_process,time_remaining,time_type=self.viewhandler.main_view_handler()
-
-        # Generate and encode graph images
-        graph_data = self.graphical_orientation.plot_graph()
+        configuration_details,current_batch,emails_to_process,time_remaining,time_type,graph_data=self.viewhandler.main_view_handler()
         encoded_graphs = {}
-        for key, buffer in graph_data.items():
-            if isinstance(buffer, BytesIO):
-                # If buffer is a BytesIO object, get its value directly
-                img_data = buffer.getvalue()
-            else:
-                # If buffer is not BytesIO, it should be handled accordingly
-                img_data = buffer
-            
-            # Encode image data to base64
-            encoded_graphs[key] = base64.b64encode(img_data).decode('utf-8') if img_data else None
+        if graph_data is None:
+             encoded_graphs=encoded_graphs
+        else:
+            for key, buffer in graph_data.items():
+                if isinstance(buffer, BytesIO):
+                    # If buffer is a BytesIO object, get its value directly
+                    img_data = buffer.getvalue()
+                else:
+                    # If buffer is not BytesIO, it should be handled accordingly
+                    img_data = buffer
+                
+                # Encode image data to base64
+                encoded_graphs[key] = base64.b64encode(img_data).decode('utf-8') if img_data else None
 
         last_refresh_time = datetime.now().strftime('%H:%M:%S')
         # Combine all context data
@@ -46,7 +49,7 @@ class View_Functions():
         
         return render(request, 'index.html', context)
     
-    def start_monitoring(self,request):
+    def start_monitoring(self,request: HttpRequest)-> HttpResponse:
         if request.method == 'POST':
             try:
                 update_yaml("config.yaml", {"STATUS":1})
@@ -54,9 +57,9 @@ class View_Functions():
                 logging.error(f"Failed to start monitoring: {e}")  # exception occurred during starting monitoring
                 raise e
             return redirect('index_view')    # Transfer control to index view
+        return redirect('index_view')
 
-
-    def stop_monitoring(self,request):
+    def stop_monitoring(self,request: HttpRequest)-> HttpResponse:
         if request.method == 'POST':
             try:
                 update_yaml("config.yaml", {"STATUS":0})
@@ -64,4 +67,4 @@ class View_Functions():
             except Exception as e:
                 logging.error(f"Failed to stop monitoring: {e}")  # exception occurred during starting monitoring
                 raise e
-        
+        return redirect('index_view')  # Transfer control to index view
